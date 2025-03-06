953b872e43ad047f3336f534a23bab6710eda1eb
Roshan <rk5970869@gmail.com>
2025-03-07 00:40:01 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -4,17 +4,31 @@ const app= express();
 
 const port=7777;
 
-
+//thsi will only handle GET call to /user
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params);
+    res.send({firstName: "Roshan", lastName:"Kumar"})
+})
+app.get("/user" , (req, res)=>{
+    console.log(req.query);
+    res.send({firstName: "Sejal", lastName:"Gadekar"})
+});
+app.post("/user", (req, res)=>{
+    res.send("Data sent successfully :) ")
+})
 app.use("/hello", (req, res)=>{
     res.send("Hello Hello Hello");
 });
 
 
 
-app.use("/crash", (req, res)=>{
+app.use("/test", (req, res)=>{
     res.send("Hello from the server!");
 });
-
+app.use("/crash", (req, res)=>{
+    res.send("server crashed due to unknown error!!!");
+});
+    //this will match alll the HTTP menthod API calls to /test
 app.use("/", (req, res)=>{
     res.send("Welcome to dashboard");
 });
