a1512186f19b77c485bd8e0dabcd17c1e82e38c2
Roshan <rk5970869@gmail.com>
2025-04-16 03:10:49 +0530

    password patch in profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,6 +3,7 @@ const User=require("../models/user.js");
 const {userAuth} =require("../middlewares/auth");
 const {validateEditProfileData}=require("../utils/validation.js");
 const profileRouter=express.Router();
+const bcrypt=require("bcrypt");
 
 profileRouter.get("/profile",userAuth,async (req, res)=>{
     try{
@@ -56,4 +57,39 @@ profileRouter.patch("/profile/edit",userAuth,async(req, res)=>{
         }
        
     });
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+        const { currentPassword, newPassword, confirmNewPassword } = req.body;
+    
+        try {
+            // Validate input
+            if (!currentPassword || !newPassword || !confirmNewPassword) {
+                return res.status(400).send("All fields must be filled");
+            }
+    
+            // Check if new password and confirmation match
+            if (newPassword !== confirmNewPassword) {
+                return res.status(400).send("New passwords entred  do not match.");
+            }
+    
+            const user = req.user;
+    
+            // Check if current password is correct
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+            if (!isMatch) {
+                return res.status(401).send("Incorrect current password.");
+            }
+    
+            // Hash the new password
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+    
+            // Update and save
+            user.password = hashedPassword;
+            await user.save();
+    
+            res.send("Password updated successfully!");
+        } catch (err) {
+            res.status(500).send("Failed to update password: " + err.message);
+        }
+    });
+    
 module.exports=profileRouter;
